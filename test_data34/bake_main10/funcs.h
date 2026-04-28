198dbcda11e6b0a5622916d8933d36d8562a7a1c
desbenj464 <desbenj3@gmail.com>
2026-04-28 03:01:35 -0400

    Add files via upload

---
+++ test_data34/bake_main10/funcs.h
@@ -0,0 +1,10 @@
+void func_01();
+void func_02();
+void func_03();
+void func_04();
+void func_05();
+void func_06();
+void func_07();
+void func_08();
+void func_09();
+void func_10();
---
+++ test_data34/bake_main10/main_func.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "funcs.h"
+int main(int argc, char *argv[]){
+  // call various library functions
+  func_01();
+  func_02();
+  func_03();
+  func_04();
+  func_05();
+  func_06();
+  func_07();
+  func_08();
+  func_09();
+  func_10();
+  return 0;
+}
